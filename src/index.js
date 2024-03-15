97a290a8d510f01cec08cf443a3bd1d9425810bc
Pallavi1502 <137083391+Pallavi1502@users.noreply.github.com>
2024-03-16 00:23:51 +0530

    catalog render code and apiconnector

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux"
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducer/combineReducers";
 
 
 const store = configureStore({
